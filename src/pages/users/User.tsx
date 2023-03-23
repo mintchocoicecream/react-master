f9d53bef502a21486812f382c0b9c7deca63ed06
hyunjeong seo <sxxhnjng@gmail.com>
2023-03-23 09:52:26 +0900

    #4.7 useOutletContext

--- src/pages/users/User.tsx
+++ src/pages/users/User.tsx
@@ -8,7 +8,9 @@ function User(){
             <h1>User with it {userId} is named: {users[Number(userId)-1].name}</h1>
             <hr/>
             <Link to="followers">See Followers</Link>
-            <Outlet />
+            <Outlet context={{
+                nameOfMyUser: users[Number(userId)-1].name,
+            }}/>
         </div>
     )
 }
