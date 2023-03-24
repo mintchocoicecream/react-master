11ff4613cf9b70b11d2fb5003d2edfff1050a25c
hyunjeong seo <sxxhnjng@gmail.com>
2023-03-24 09:43:19 +0900

    #5.1 Styles
    reset, createGlobalStyle

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,9 @@
 import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme={
-    bgColor: "white",
-    textColor: "black",
-    btnColor: "pink",
+    bgColor: "#2f3640",
+    textColor: "#f5f6fa",
+    accentColor: "#9c88ff",
 }
 
 // export const darkThem: DefaultTheme={
