9a25796e79c3230fffa35dbb934daf54664e9b9a
hyunjeong seo <sxxhnjng@gmail.com>
2023-03-27 16:26:50 +0900

    #5.8-9 Nested Routes

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { Link, Outlet, useLocation, useMatch, useParams } from "react-router-dom";
 import styled from "styled-components";
 
 const Container=styled.div`
@@ -24,6 +24,58 @@ const Title = styled.h1`
 const Loader = styled.span`
   text-align: center;
   display: block;
+`;
+
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0,0,0,0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+
+const OverviewItem =styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    span:first-child{
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    }
+`;
+
+const Descriptsion=styled.p`
+    margin: 20px 0px;
+`;
+
+const Tabs=styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+//Tab styled component는 isActive라고 불리는 prop을 가질 것.
+const Tab=styled.span<{isActive:boolean}>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0,0,0,0.5);
+    border-radius: 10px;
+    transition: background-color .2s ease-in;
+    cursor: pointer;
+    color: ${props => props.isActive ? props.theme.accentColor : props.theme.textColor };
+    &:hover{
+            background-color: rgba(0,0,0);
+        }
+    a{
+        padding: 7px 0px;
+        display: block;
+    }
 `;
 
 interface RouteParams{
@@ -96,7 +148,12 @@ function Coin(){
     const {state}=useLocation() as RouterState;
     const [info, setInfo]=useState<InfoData>();
     const [priceInfo, setPriceInfo]=useState<PriceData>();
+
+    // useMatch(URL): 해당 URL에 있는 지 알려줌
+    const priceMatch=useMatch(":coinId/price");
+    const chartMatch=useMatch(":coinId/chart");
     useEffect(()=>{
+        
         (async()=>{
             const infoData=await axios(`https://api.coinpaprika.com/v1/coins/${coinId}`);
             const priceData=await axios({
@@ -109,15 +166,53 @@ function Coin(){
             // console.log(infoData.data);
             // console.log(priceData);
             setInfo(infoData.data);
-            setPriceInfo(priceData.data);
+            setPriceInfo(priceData);
+            setLoading(false);
         })();
-    },[]);
+    },[coinId]);
     return (
     <Container>
         <Header>
-            <Title>{state? state:"Loading..."}</Title>
+            <Title>{state? state: loading? "Loading...":info?.name}</Title>
         </Header>
-        {loading? <Loader>Loading...</Loader> : null}
+        {loading? <Loader>Loading...</Loader> : (
+        <>
+            <Overview>
+                <OverviewItem>
+                    <span>Rank:</span>
+                    <span>{info?.rank}</span>
+                </OverviewItem>
+                <OverviewItem>
+                    <span>Symbol:</span>
+                    <span>${info?.symbol}</span>
+                </OverviewItem>
+                <OverviewItem>
+                    <span>Open Source:</span>
+                    <span>{info?.open_source ? "Yes":"No"}</span>
+                </OverviewItem>
+            </Overview>
+            <Descriptsion>{info?.description}</Descriptsion>
+            <Overview>
+            <OverviewItem>
+              <span>Total Suply:</span>
+              <span>{priceInfo?.total_supply}</span>
+            </OverviewItem>
+            <OverviewItem>
+              <span>Max Supply:</span>
+              <span>{priceInfo?.max_supply}</span>
+            </OverviewItem>
+          </Overview>
+          <Tabs>
+            <Tab isActive={chartMatch !==null}>
+                <Link to="chart">Chart</Link>
+            </Tab>
+            <Tab isActive={priceMatch !==null}>
+                <Link to="price">Price</Link>
+            </Tab>
+          </Tabs>
+          <Outlet/>
+        </>
+        )}
     </Container>
     )
 }
