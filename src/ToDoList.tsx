42bf20cb0ea574e2b4116fd55af73f839d5dc6c8
hyunjeong seo <sxxhnjng@gmail.com>
2023-03-30 16:46:14 +0900

    #6.10 Recap
    Add setValue

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,69 +1,29 @@
 import {useForm} from "react-hook-form";
 
-interface IFormData {
-    // errors: {
-    //     email: {
-    //         message: string;
-    //     };
-
-    // };
-    email: string;
-    name: string;
-    password: string;
-    password1: string;
-    extraError?:string;
-}
+interface IForm{
+    toDo:string;
+};
 
 export default function ToDoList() {
-    const {register, handleSubmit, formState:{errors}, setError}=useForm<IFormData>({
-        defaultValues: {
-            email: "@naver.com",
-        }
-    });
-
-    // react-hook-form이 모든 validation을 다 마쳤을 때만 호출될 것
-    const onValid=(data:IFormData)=>{
-        if(data.password !== data.password1){
-            return setError("password1", {message: "Password are not the same!"});
-        };
-        //전체 form에 해당되는 에러
-        // setError("extraError", {message:"server offline..."});
-    };
-
-    // console.log(errors?.email?.message);
+    //handleSubmit을 사용할 때 첫번째 매개변수로 데이터가 유효할 때
+        //호출되는 다른 함수를 받는다.
+    //setValue: submit후에 value 비우기
+    const {
+        register, handleSubmit, setValue
+    }=useForm<IForm>();
+    const handleValid=(data: IForm)=>{
+        console.log("add to do", data.toDo);
+        setValue("toDo", "");
+    }
     return(
         <div>
-            <form style={{display:"flex", flexDirection:"column"}} onSubmit={handleSubmit(onValid)}>
-                <input 
-                {...register("email", {
-                    required: "Email is required", 
-                    pattern: {
-                    value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-                    message: "Only naver.com emails allowed.",
-                },
+            <form onSubmit={handleSubmit(handleValid)}>
+                <input {...register("toDo", {
+                    required: "Please Write a Todo",
                 })} 
-                placeholder='Email'/>
-                <span style={{ color: errors?.email?.message ? "red" : "" }}>{errors?.email?.message}</span>
-                <input {...register("name", {
-                    required: "write here", 
-                    validate: (value)=>!value.includes("nico")?true:"error message",
-                    },
-                    )} 
-                    placeholder='name'/>
-                <span style={{ color: errors?.name?.message ? "red" : "" }}>{errors?.name?.message}</span>
-                <input {...register("password", {required:"write here", minLength:10})} placeholder='password'/>
-                <span style={{ color: errors?.password?.message ? "red" : "" }}>{errors?.password?.message}</span>
-                <input {...register("password1", {
-                    required: "Password is required", 
-                    minLength:{
-                        value: 5,
-                        message:"Your password is too short!"
-                    },
-                    })} placeholder='password check'/>
-                <span style={{ color: errors?.password1?.message ? "red" : "" }}>{errors?.password1?.message}</span>
+                placeholder="Write here"/>
                 <button>Add</button>
-                <span style={{ backgroundColor: errors?.extraError?.message ? "red" : "" }}>{errors?.extraError?.message}</span>
             </form>
-        </div> 
+        </div>
     )
 };
